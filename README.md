33bd920a2b531aee4e7d29c1bb6fea9ec5985b6a
Ivan Koposhilov <ikoposhilov@yandex.ru>
2022-06-29 21:27:20 +0500

    create repozitory, update README, requirements, gitignore, add script for create database

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # excel_to_mysql
 collecting data from excel to mysql
+
+## Скрипт для создания базы данных MySQL и заполение ее данными с excel
---
+++ excel_sql.py
@@ -0,0 +1,74 @@
+import mysql.connector
+from mysql.connector import errorcode
+import openpyxl
+import time
+from settings import user, password, path
+
+start = time.time()
+conn = mysql.connector.connect(user=user,  password=password)
+cursor = conn.cursor()
+
+DB_NAME = 'price'
+TABLES = {}
+TABLES['price'] = (
+    "CREATE TABLE `price` (`id` int(20) NOT NULL AUTO_INCREMENT,"
+    "`description` TEXT,"
+    "`qty` int(11),"
+    "`part_number` varchar(25),"
+    "`articul` varchar(25),"
+    "`price` decimal(10,2),"
+    "`ea` varchar(14),"
+    "PRIMARY KEY (`id`)"
+") ENGINE=InnoDB")
+
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+try:
+    cursor.execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        print("Database {} created successfully.".format(DB_NAME))
+        conn.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
+
+for table_name in TABLES:
+    table_description = TABLES[table_name]
+    try:
+        print("Creating table {}: ".format(table_name), end='')
+        cursor.execute(table_description)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
+
+wb = openpyxl.load_workbook(path)
+sheet = wb.active
+nb_row = sheet.max_row
+
+for i in range (2, nb_row):
+    description = sheet[f'B{i}'].value
+    qty = sheet[f'C{i}'].value
+    part_number = sheet[f'D{i}'].value
+    articul = sheet[f'E{i}'].value
+    price = sheet[f'F{i}'].value
+    ea = sheet[f'G{i}'].value
+    cursor.execute("insert into `price`(description, qty, part_number, articul, price, ea) values(%s, %s, %s, %s, %s, %s)", (description, qty, part_number, articul, price, ea))
+
+conn.commit()
+cursor.close()
+conn.close()
+end = time.time()
+print(end-start)
