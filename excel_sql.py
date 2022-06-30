aa39eec329386cd5f35c7d1a5e1f6117414b2bb5
Ivan Koposhilov <ikoposhilov@yandex.ru>
2022-06-30 21:34:57 +0500

    update script, change collect data

--- excel_sql.py
+++ excel_sql.py
@@ -58,14 +58,28 @@ wb = openpyxl.load_workbook(path)
 sheet = wb.active
 nb_row = sheet.max_row
 
-for i in range (2, nb_row):
-    description = sheet[f'B{i}'].value
-    qty = sheet[f'C{i}'].value
-    part_number = sheet[f'D{i}'].value
-    articul = sheet[f'E{i}'].value
-    price = sheet[f'F{i}'].value
-    ea = sheet[f'G{i}'].value
-    cursor.execute("insert into `price`(description, qty, part_number, articul, price, ea) values(%s, %s, %s, %s, %s, %s)", (description, qty, part_number, articul, price, ea))
+# data = [[
+# sheet[f'B{i}'].value,
+# sheet[f'C{i}'].value,
+# sheet[f'D{i}'].value,
+# sheet[f'E{i}'].value,
+# sheet[f'F{i}'].value,
+# sheet[f'G{i}'].value
+# ]
+# for i in range (2, nb_row)
+# ]
+
+data = list(sheet.iter_rows(min_row=2, max_row=nb_row, max_col=6, values_only=True))
+cursor.executemany("insert into `price`(description, qty, part_number, articul, price, ea) values(%s, %s, %s, %s, %s, %s)", data)
+
+# for i in range (2, nb_row):
+#     description = sheet[f'B{i}'].value
+#     qty = sheet[f'C{i}'].value
+#     part_number = sheet[f'D{i}'].value
+#     articul = sheet[f'E{i}'].value
+#     price = sheet[f'F{i}'].value
+#     ea = sheet[f'G{i}'].value
+#     cursor.execute("insert into `price`(description, qty, part_number, articul, price, ea) values(%s, %s, %s, %s, %s, %s)", (description, qty, part_number, articul, price, ea))
 
 conn.commit()
 cursor.close()
